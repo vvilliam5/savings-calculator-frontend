3c9a34bba622a73caefb7b95c03b8de646a382ab
williamswill <willoymaniak@gmail.com>
2020-08-13 02:15:26 +0100

    fixed login issue

--- src/components/Auth/LoginForm.vue
+++ src/components/Auth/LoginForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" @submit="loginAttempt">
+  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" @submit.prevent="loginAttempt">
     <h2 class="text-center text-xl font-semibold pb-4">LOG IN</h2>
     <Alert @closePopUp="closeError()" v-if="loginError" :text="loginErrorMessage"></Alert>
     <div class="mb-4">
