d6067a500247c075b96ca1ed24cb705e2033269b
williamswill <willoymaniak@gmail.com>
2020-08-13 02:25:12 +0100

    fixed passwprd input field

--- src/components/Auth/SignUpForm.vue
+++ src/components/Auth/SignUpForm.vue
@@ -27,7 +27,7 @@
     <div class="mb-4">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
       <input
-        class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         id="password"
         type="password"
         placeholder="Password"
@@ -39,7 +39,7 @@
     <div class="mb-6">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="cpassword">Confirm Password</label>
       <input
-        class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
         id="cpassword"
         type="password"
         placeholder="Password"
